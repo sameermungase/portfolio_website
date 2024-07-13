3383c727b2aca85b02ce2fc8763120072025aa70
Sameer Mungase <143208446+sameermungase@users.noreply.github.com>
2024-07-13 23:28:50 +0530

    Update portfolio_website.py

--- portfolio_website.py
+++ portfolio_website.py
@@ -67,14 +67,28 @@ st.title("My Skills")
 st.slider("DSA", 0, 100, 70)
 st.slider("Core Java", 0, 100, 85)
 st.slider("Linux", 0, 100, 75)
+st.slider("Python", 0 ,100 ,50)
+st.slider("Generative AI" ,0 ,100 ,50)
 
 st.title(" ")
 
-st.title("About My College")
+st.title("My College")
 
 st.image("imagee/geca.jpeg", width=700)
 
+st.title("My Projects")
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("My Electronics Projects")
+    st.write("- Home Appliances Automation")
+    st.write("- Trinetra (a device which will help visually impared persons detect obstable")
+    st.write("- FIR filters")
 
+with col2:
+    st.subheader("My Coding Projects")
+    st.write("AI chatbot")
+    st.write("Hotel Management System")
+    
 st.title(" ")
 
 
